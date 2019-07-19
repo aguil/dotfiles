a4f5177566ab3c739e94cdc9e9fa05ac16758a7f
Jason Aguilon <jaguilon@gmail.com>
2019-07-19 16:32:58 -0600

    cask install apps

--- install.sh
+++ install.sh
@@ -36,34 +36,3 @@ pip install virtualenvwrapper
 export WORKON_HOME=~/virtualenvs
 mkdir -p $WORKON_HOME
 
-# Anaconda
-# From https://docs.anaconda.com/anaconda/install/mac-os/
-curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-MacOSX-x86_64.sh
-./verify.sh /Anaconda3-2019.03-MacOSX-x86_64.sh 46709a416be6934a7fd5d02b021d2687
-EXITCODE = $?
-if [ $EXITCODE -ne 0 ]; then
-  exit $EXITCODE
-fi
-# NOTE: Heads up as Anaconda's install shell script requires some interaction.
-bash ./Anaconda3-2019.03-MacOSX-x86_64.sh
-
-echo "Apps this script doesn't install:
-Alfred
-Better Touch Tool
-Box
-Disk Inventory X
-Docker
-Dropbox
-Evernote
-Firefox
-GeekTool
-Gimp
-IntelliJ IDEA
-iTerm2
-kdiff3
-LibreOffice
-MacVim
-p4merge
-Slack
-Wireshark
-"
