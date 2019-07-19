cb1566af85c4bcf901c9712e250b6eab806a36d1
Jason Aguilon <jaguilon@gmail.com>
2019-07-19 08:54:49 -0600

    dropping bpython

--- install.sh
+++ install.sh
@@ -11,19 +11,19 @@ https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /usr/local
 brew bundle
 
 # setup dotfiles using homeshick.
+echo "Setting up dotfiles..."
 homeshick clone aguil/dotfiles
 homeshick check
 
 # python setuptools -- pip requirement
+echo "Python 2.7: setuptools"
 curl -o setuptools-0.6c11-py2.7.egg https://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11-py2.7.egg#md5=fe1f997bc722265116870bc7919059ea
 sh setuptools-0.6c11-py2.7.egg
 
 # pip
+echo "Python 2.7: pip"
 curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 python get-pip.py
-
-# bpython
-pip install bpython
 
 # Powerline
 pip install --user git+git://github.com/Lokaltog/powerline
